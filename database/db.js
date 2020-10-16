b101e476394b46d108be6fdad79e8e957a016708
liranal <liraneilary@gmail.com>
2020-10-16 19:57:17 +0300

    dotenv and heroku

--- database/db.js
+++ database/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
 //Set up default mongoose connection
-const mongoDB = "mongodb://localhost:27017/UsersDB";
+//const mongoDB = "mongodb://localhost:27017/UsersDB";
+const mongoDB = process.env.MONGO_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 //Get the default connection
 const db = mongoose.connection;
